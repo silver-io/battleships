7e4091570305ecbe349d16825a3c563e0096b8a7
Henry Stanley <henry@henrystanley.com>
2014-08-26 17:52:33 +0100

    Added methods and tests for Ship

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,12 @@
 class Ship
 
+	def initialize(options = {})
+		
+		@coordinates = options.fetch(:coordinates, "something")
+	end
+
+	def coordinates
+		@coordinates
+	end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -4,7 +4,13 @@ describe Ship do
 
 	let(:ship) { Ship.new }
 
-	xit "has a position when initialized" do
-		expect(ship.position).not_to be nil
+	it "should have co-ordinates" do
+		expect(ship.coordinates).not_to eq nil
 	end
+
+	it "should returns its co-ordinates when asked" do
+		ship1 = Ship.new({:coordinates => [:A1,:B1]})
+		expect(ship1.coordinates).to eq [:A1, :B1]
+	end
+
 end

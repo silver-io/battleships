8c13fab6fd355da29d50bb272db6db7749fa1030
NineInchNade <nineinchnade@gmail.com>
2014-08-27 12:02:11 +0100

    ship_new

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,7 @@ class Board < Hash # ensures Board is itself a type of Hash
 				coordinate = "#{letter}#{number}"
 				self[coordinate.to_sym] = Cell.new
 			end
+			
 		end
 		
 	end
--- lib/ship.rb
+++ lib/ship.rb
@@ -1,12 +1,27 @@
 class Ship
 
-	def initialize(options = {})
-		
-		@coordinates = options.fetch(:coordinates, "something")
+	def initialize(size=3)
+		@size = size 
+		@hitpoints = size 
+	end 
+
+	def size 
+		@size 
+	end 
+
+	def hp? 
+		@hitpoints 
+	end 
+
+	def hit! 
+		raise "this ship already sunk" if @hitpoints == 0 
+		@hitpoints -= 1 
+	end 
+
+	def sunk? 
+		@hitpoints == 0 
 	end
 
-	def coordinates
-		@coordinates
-	end
+	
 
 end

1087bd0c574bb80a09f8b5d6879f885fd32c1094
javier <js@silveriofunk.com>
2014-08-28 11:09:47 +0100

    ships have subclasses

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,6 +1,6 @@
 class Ship
 
-	def initialize(size=3)
+	def initialize(size)
 		@size = size 
 		@hitpoints = size 
 	end 
@@ -21,9 +21,50 @@ class Ship
 	def sunk? 
 		@hitpoints == 0 
 	end
-
-
-
 	
-
 end
+
+class Fisherboat < Ship
+
+	def initialize(size=1)
+		super
+	end
+end
+
+class Patrol_boat < Ship
+
+	def initialize(size=2)
+		super
+	end
+end
+
+class Destroyer < Ship 
+	
+	def initialize(size=3)
+		super
+	end 
+end
+
+class Battleship < Ship
+
+	def initialize(size=4)
+		super
+	end 
+end
+
+class Aircraft_carrier < Ship
+
+	def initialize(size=5)
+		super
+	end
+end 
+
+
+
+
+#SHIP_TYPES = {:tiny => 1,
+#	:patrol_boat => 2,
+#	:destroyer => 3,
+#	:submarine => 3,
+#	:Battleship => 4,
+#	:aircraft_carrier => 5}
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,44 +2,82 @@ require 'ship'
 
 describe Ship do
 
-	let(:ship) { Ship.new }
+	let(:ship)          	{ Ship.new(20) }
+	let(:fisherboat)		{ Fisherboat.new }
+	let(:patrol_boat)		{ Patrol_boat.new }
+	let(:destroyer)     	{ Destroyer.new }
+	let(:battleship)	  	{ Battleship.new }
+	let(:aircraft_carrier)	{ Aircraft_carrier.new }
 
-	it 'has a size' do 
-		expect(ship.size.nil?).to be false 
+		context 'ship' do 
+
+		it 'has a size' do 
+			expect(ship.size.nil?).to be false 
+		end
+
+		it 'can have different sizes' do
+			expect(patrol_boat.size).to eq 2 
+			expect(aircraft_carrier.size).to eq 5 
+		end 
+
+		it 'has hitpoints equal to their size' do 
+			expect(ship.hp?).to eq 20
+			expect(patrol_boat.hp?).to eq 2
+			expect(aircraft_carrier.hp?).to eq 5
+		end 
+
+		it 'can lose a hitpoint when hit' do 
+			ship.hit! 
+			expect(ship.hp?).to eq 19 
+		end 
+
+		it 'sinks if out of hitpoints' do 
+			20.times{ ship.hit! }
+			expect(ship.sunk?).to be true 
+		end
+
+		it 'can\'t be hit if sunk' do 
+			20.times{ ship.hit! }
+			expect{ ship.hit! }.to raise_error(RuntimeError)
+		end 
 	end
 
-	it 'has the size 3 per defautl' do
-		expect(ship.size).to eq 3 
-	end 
+	context 'fisherboat' do
 
-	it 'can have different sizes' do
-		small_ship = Ship.new(2)
-		big_ship = Ship.new(5)
-		expect(small_ship.size).to eq 2 
-		expect(big_ship.size).to eq 5 
-	end 
-
-	it 'has hitpoints equal to their size' do 
-		expect(ship.hp?).to eq 3 
-		small_ship = Ship.new(2)
-		expect(small_ship.hp?).to eq 2
-		big_ship = Ship.new(5) 
-		expect(big_ship.hp?).to eq 5
-	end 
-
-	it 'can lose a hitpoint when hit' do 
-		ship.hit! 
-		expect(ship.hp?).to eq 2 
-	end 
-
-	it 'sinks if out of hitpoints' do 
-		3.times{ ship.hit! }
-		expect(ship.sunk?).to be true 
+		it 'has a size of 1' do 
+			expect(fisherboat.size).to eq 1 
+		end 
 	end
 
-	it 'can\'t be hit if sunk' do 
-		3.times{ ship.hit! }
-		expect{ ship.hit! }.to raise_error(RuntimeError)
+	context 'patrol boat' do 
+
+		it 'has a size of 2' do 
+			expect(patrol_boat.size).to eq 2 
+		end 
 	end 
+
+	context 'destroyer' do
+	
+		it 'has a size of 3' do
+			expect(destroyer.size).to eq 3
+		end
+	end
+
+	context 'battle_ship' do
+
+		it 'has a size of 4' do 
+			expect(battleship.size).to eq 4 
+		end 
+	end 
+
+	context 'aircraft_carrier' do
+
+		it 'has a size of 5' do
+			expect(aircraft_carrier.size).to eq 5
+		end
+	end
+
+
+
 
 end 
